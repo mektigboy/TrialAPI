716f7651ce31c4091c8f52f84e0d899ec1598b54
mektigboy <mektigboy@protonmail.com>
2022-08-25 18:05:51 -0500

    Update src/components/Converter.tsx

--- src/components/Converter.tsx
+++ src/components/Converter.tsx
@@ -8,27 +8,40 @@ interface ConverterProps {
   network: string;
 }
 
-export const Converter: React.FC<ConverterProps> = ({ network }) => {
+export const Converter: React.FC<ConverterProps> = () => {
   const wallet = useWallet();
 
   const { connection } = useConnection();
 
-  const [amountFrom, setAmountFrom] = useState<number>(1);
-  const [amountTo, setAmountTo] = useState<number>(1);
+  const [amount, setAmount] = useState<number>(1);
+  const [slippage, setSlippage] = useState<number>(0.5);
 
-  const onAmountFromChange = (event: ChangeEvent<any>) => {
-    setAmountFrom(event.target.value);
+  const [tokenFrom, setTokenFrom] = useState<string>("SOL");
+  const [tokenTo, setTokenTo] = useState<string>("COSMIC");
+
+  const onAmountChange = (event: ChangeEvent<any>) => {
+    setAmount(event.target.value);
   };
-  const onAmountToChange = (event: ChangeEvent<any>) => {
-    setAmountTo(event.target.value);
+  const onSlippageChange = (event: ChangeEvent<any>) => {
+    setSlippage(event.target.value);
   };
 
-  const amountToSend = amountFrom * 1e9;
+  const amountToSend = amount * 1e9;
+
+  const retrieveRouteMap = async () => {
+    const indexedRouteMap = await (
+      await fetch("https://quote-api.jup.ag/v1/indexed-route-map")
+    ).json();
+    return indexedRouteMap;
+  };
+
+  const indexedRouteMap = retrieveRouteMap();
+  console.log(indexedRouteMap);
 
   const swapTokens = async () => {
     const { data } = await (
       await fetch(
-        `https://quote-api.jup.ag/v1/quote?inputMint=So11111111111111111111111111111111111111112&outputMint=NGK3iHqqQkyRZUj4uhJDQqEyKKcZ7mdawWpqwMffM3s&amount=${amountToSend}&slippage=0.5&feeBps=1`
+        `https://quote-api.jup.ag/v1/quote?inputMint=So11111111111111111111111111111111111111112&outputMint=NGK3iHqqQkyRZUj4uhJDQqEyKKcZ7mdawWpqwMffM3s&amount=${amountToSend}&slippage=${slippage}&feeBps=1`
       )
     ).json();
     const routes = data;
@@ -63,31 +76,59 @@ export const Converter: React.FC<ConverterProps> = ({ network }) => {
         skipPreflight: true,
       });
       console.log(`https://solscan.io/tx/${txid}`);
+      const latestBlockHash = await connection.getLatestBlockhash();
+      await connection.confirmTransaction({
+        blockhash: latestBlockHash.blockhash,
+        lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+        signature: txid,
+      });
     }
   };
 
   return (
     <div>
       <h1 className="mb-25">Convert</h1>
-      <h2 className="mb-25">$SOL to $YAKU</h2>
+      <h3 className="mb-25">
+        ${tokenFrom} to ${tokenTo}
+      </h3>
       {!wallet.connected && <WalletMultiButton />}
       <div className="mt-25 mb-25">
         <label>From:</label>
         <div className="input">
-          <input onChange={onAmountFromChange} value={amountFrom} />
-          <div className="token">$SOL</div>
+          <input
+            className="token-input"
+            onChange={onAmountChange}
+            value={amount}
+          />
+          <div className="token">${tokenFrom}</div>
         </div>
       </div>
       <div className="mb-25">
         <label>To:</label>
         <div className="input">
-          <input onChange={onAmountToChange} value={amountFrom * 2954.8} />
-          <div className="token">$YAKU</div>
+          <input
+            className="token-input"
+            onChange={onAmountChange}
+            value={amount * 2954.8}
+          />
+          <div className="token">${tokenTo}</div>
         </div>
       </div>
       <div className="mt-50 mb-50">
-        <div className="mb-25">Transaction Settings</div>
-        <div className="mb-25">Slippage Tolerance: 0.5%</div>
+        <div className="mb-25">
+          <h4>Transaction Settings</h4>
+        </div>
+        <div className="slippage mb-25">
+          Slippage Tolerance:
+          <div className="slippage-input-group">
+            <input
+              className="slippage-input"
+              onChange={onSlippageChange}
+              value={slippage}
+            />
+            <div className="ml-10">%</div>
+          </div>
+        </div>
         <div className="mb-25">Allowance: Exact Amount</div>
         <div className="mb-25">Swap Fee: 0.01%</div>
       </div>
