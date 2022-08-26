c07ec8329780cd01ad53b930d84dc7edfcb305d8
mektigboy <mektigboy@protonmail.com>
2022-08-26 12:49:23 -0500

    Update src/constants.ts

---
+++ src/constants.ts
@@ -0,0 +1,38 @@
+import { Cluster } from "@solana/web3.js";
+import { ENV as ENVChainId } from "@solana/spl-token-registry";
+
+require("dotenv").config();
+
+export const ENV: Cluster =
+  (process.env.NEXT_PUBLIC_CLUSTER as Cluster) || "mainnet-beta";
+export const CHAIN_ID =
+  ENV === "mainnet-beta"
+    ? ENVChainId.MainnetBeta
+    : ENV === "devnet"
+    ? ENVChainId.Devnet
+    : ENV === "testnet"
+    ? ENVChainId.Testnet
+    : ENVChainId.MainnetBeta;
+export const SOLANA_RPC_ENDPOINT =
+  ENV === "devnet"
+    ? "https://api.devnet.solana.com"
+    : "https://solana-api.projectserum.com";
+
+export const INPUT_MINT_ADDRESS =
+  ENV === "devnet"
+    ? "So11111111111111111111111111111111111111112" // SOL
+    : "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"; // USDC
+export const OUTPUT_MINT_ADDRESS =
+  ENV === "devnet"
+    ? "SRMuApVNdxXokk5GT7XD5cUUgXMBCoAz2LHeuAoKWRt" // SRM
+    : "Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB"; // USDT
+
+export interface Token {
+  chainId: number;
+  address: string;
+  symbol: string;
+  name: string;
+  decimals: number;
+  logoURI: string;
+  tags: string[];
+}
